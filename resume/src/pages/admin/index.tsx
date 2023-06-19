5fe39f548986f75963ed20959627f49f61570eee
Edwin M <edwingmundo2@gmail.com>
2023-06-19 05:26:57 -0400

    added a table of all sessions

--- resume/src/pages/admin/index.tsx
+++ resume/src/pages/admin/index.tsx
@@ -9,6 +9,7 @@ const AdminIndex = () => {
     api.session.get_avg_session_duration.useQuery();
   const total_session =
     api.session.get_total_session_duration.useQuery();
+  const all_sessions = api.session.get_all.useQuery();
   return (
     <>
       <div className="flex flex-row justify-center">
@@ -49,7 +50,7 @@ const AdminIndex = () => {
           </p>
         </div>
       </div>
-      <div className="pl-10 pr-10">
+      <div className="flex flex-row pl-10 pr-10">
         <table className="table-auto border-collapse border border-slate-400 text-white">
           <thead>
             <tr>
@@ -70,6 +71,36 @@ const AdminIndex = () => {
                 <td className="border border-slate-300 pl-5 pr-5 text-center">
                   {record.count}
                 </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <table className="ml-10 table-auto border-collapse border border-slate-400 text-white">
+          <thead>
+            <tr>
+              <th className="border border-slate-300 pl-5 pr-5">
+                Session IP
+              </th>
+              <th className="border border-slate-300 pl-5 pr-5">
+                Session User Agent
+              </th>
+              <th className="border border-slate-300 pl-5 pr-5">
+                Session Duration
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {all_sessions.data?.map((record, index) => (
+              <tr key={index}>
+                <th className="border border-slate-300 pl-5 pr-5">
+                  {record.ipAddress.ipAddress}
+                </th>
+                <th className="border border-slate-300 pl-5 pr-5">
+                  {record.userAgent}
+                </th>
+                <th className="border border-slate-300 pl-5 pr-5">
+                  {record.sessionDuration}
+                </th>
               </tr>
             ))}
           </tbody>
